735c0f579f6e4f5c7e8e1ea854ea9d806ca4dfa1
Yuriy <junker098@gmail.com>
2024-05-06 18:32:02 +0300

    Initial Commit

---
+++ RaMApp/RaMAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaMAppApp.swift
+//  RaMApp
+//
+//  Created by Yuriy on 06.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct RaMAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
