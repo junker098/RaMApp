c5d13030d19a62df56cef4df244abfef50770a4c
Yuriy <junker098@gmail.com>
2024-05-07 23:15:31 +0300

    add image modifier

--- RaMApp/Core/Modifiers/ProfileImageModifier.swift
+++ RaMApp/Core/Modifiers/ProfileImageModifier.swift
@@ -5,4 +5,17 @@
 //  Created by Yuriy on 07.05.2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct ProfileImageModifier: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        content
+            .frame(width: 200, height: 200)
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.green, lineWidth: 3)
+            }
+            .shadow(color: Color.yellow.opacity(0.8), radius: 1, x: 0, y: 2)
+    }
+}
