3b75c77d4c749360ef592ae14d6886d0027f534a
Yuriy <junker098@gmail.com>
2024-05-07 23:15:19 +0300

    add detail screen

--- RaMApp/Core/DetailView/CharacterDetailView.swift
+++ RaMApp/Core/DetailView/CharacterDetailView.swift
@@ -6,13 +6,81 @@
 //
 
 import SwiftUI
+import NukeUI
 
 struct CharacterDetailView: View {
+    
+    var character: Character
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Rectangle()
+                .fill(
+                    LinearGradient(colors: [.blue, .purple], startPoint: .top, endPoint: .bottom)
+                )
+                .frame(maxWidth: .infinity, maxHeight: 250)
+            
+            VStack {
+                LazyImage(url: character.image) { state in
+                    if let image = state.image {
+                        image
+                            .resizable()
+                    } else {
+                        Image(systemName: "person.crop.circle")
+                            .resizable()
+                    }
+                }
+                .modifier(ProfileImageModifier())
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Name: ")
+                            .fontWeight(.medium)
+                            .foregroundStyle(.green)
+                        Text(character.name)
+                            .fontWeight(.regular)
+                            .foregroundStyle(.black)
+                    }
+                    .font(.title)
+                    Divider()
+                    HStack {
+                        Text("Status: ")
+                        Text(character.status)
+                            .foregroundStyle(.black)
+                    }
+                    Divider()
+                    HStack {
+                        Text("Species: ")
+                        Text(character.species)
+                            .foregroundStyle(.black)
+                    }
+                    Divider()
+                    HStack {
+                        Text("Type: ")
+                        Text(character.type.isEmpty ? "---" : character.type)
+                            .foregroundStyle(.black)
+                    }
+                    Divider()
+                    HStack {
+                        Text("Gender: ")
+                        Text(character.gender)
+                            .foregroundStyle(.black)
+                    }
+                }
+                .font(.title2)
+                .foregroundStyle(.blue)
+                .padding()
+                .background(.white)
+                .cornerRadius(10)
+                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                .padding()
+                
+            }
+            .offset(y: -110)
+        }
+        .ignoresSafeArea()
     }
 }
 
 #Preview {
-    CharacterDetailView()
+    CharacterDetailView(character: Character.testCharacter)
 }
