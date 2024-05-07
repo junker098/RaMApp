67ccc919f4c248c81511ca43d68203762c84f6af
Yuriy <junker098@gmail.com>
2024-05-07 23:14:38 +0300

    add reducer

--- RaMApp/Core/Root/Reducer/CharactersListReducer.swift
+++ RaMApp/Core/Root/Reducer/CharactersListReducer.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import ComposableArchitecture
+
+@Reducer
+struct CharactersListReducer {
+    @ObservableState
+    struct State: Equatable {
+        var characters: [Character] = []
+        var loadingStatus: LoadingStatus = .start
+        var pageLimit: Int = 10
+        var page: Int = 1
+        var errorMessage = ""
+    }
+    
+    enum Action {
+        case loadCharacters
+        case loadCharactersResponse(Result<CharacterResponse, ApiError>)
+    }
+    
+    @Dependency(\.networkService) var networkService
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .loadCharacters:
+                guard state.loadingStatus != .loading && state.page <= state.pageLimit else { return .none }
+                state.loadingStatus = .loading
+                let next = state.page
+                DebugLogger.shared.logEvent(type: .debug, object: ("run \(next) \(state.pageLimit)"))
+                return .run { send in
+                    let data = try await networkService.getAllCharacters(next)
+                    await send(.loadCharactersResponse(data))
+                }
+                
+            case .loadCharactersResponse(.success(let characterResponse)):
+                state.loadingStatus = .success
+                state.characters += characterResponse.results
+                state.pageLimit = characterResponse.info.pages
+                state.page += 1
+                return .none
+                
+            case .loadCharactersResponse(.failure(let error)):
+                state.loadingStatus = .error
+                state.errorMessage = error.localizedDescription
+                return .none
+            }
+        }
+    }
+}
