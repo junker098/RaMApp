078f0b38851838ddd700b581d2a787a3fee2aaaf
Yuriy <junker098@gmail.com>
2024-05-07 23:13:07 +0300

    add model

--- RaMApp/Model/CharacterModel.swift
+++ RaMApp/Model/CharacterModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+struct CharacterResponse: Codable {
+    let results: [Character]
+    let info: Info
+}
+
+struct Character: Codable, Identifiable, Equatable {
+    let id: Int
+    let name: String
+    let image: URL?
+    let status: String
+    let species: String
+    let type: String
+    let gender: String
+}
+
+struct Info: Codable, Equatable {
+    let pages: Int
+}
+
+extension Character {
+    static let testCharacter = Character(id: 1,
+                                         name: "Toxic Rick", image: nil,
+                                         status: "Dead",
+                                         species: "Humanoid",
+                                         type: "Rick's Toxic Side",
+                                         gender: "Male"
+    )
+}
